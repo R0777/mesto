3eeac5f06664649877827ba16de8d3c73bb41928
Ro777 <romka256@yandex.ru>
2020-06-28 14:49:57 +0300

    refactor: js and html

--- js/index.js
+++ js/index.js
@@ -1,14 +1,37 @@
 const proFile = document.querySelector('.profile')
 const popUp = document.querySelector('.popup')
+const popUpAdd = document.querySelector('.popup-add')
+const popUpBig = document.querySelector('.popup-big')
 const editButton = proFile.querySelector('.profile__edit')
 const closePop = popUp.querySelector('.popup__close')
 const popSave = popUp.querySelector('.popup__save')
+const closePopAdd = popUpAdd.querySelector('.popup-add__close')
+const popSaveAdd = popUpAdd.querySelector('.popup-add__save')
+const addButton = proFile.querySelector('.profile__button')
+const popUpAddForm = popUpAdd.querySelector('.popup-add__form')
+const closePopBig = popUpBig.querySelector('.popup-big__close');
 
-const openPop = function() {
+const openPop = function () {
   popUp.classList.toggle('popup-open');
 }
 
-const saveButt = function(ev) {
+const openPopAdd = function () {
+  popUpAdd.classList.toggle('popup-add-open');
+}
+
+const openPopBigClose = function () {
+  popUpBig.classList.toggle('popup-big-open');
+}
+
+const openPopBig = function (pic, place) {
+  const popUpBigPic = popUpBig.querySelector('.popup-big__pic')
+  const popUpBigText = popUpBig.querySelector('.popup-big__place')
+  popUpBigPic.setAttribute('src', pic);
+  popUpBigPic.setAttribute('alt', place);
+  popUpBigText.innerHTML = place
+}
+
+const saveButt = function (ev) {
   ev.preventDefault()
   let popName = popUp.querySelector('.popup__input_name').value
   let popJob = popUp.querySelector('.popup__input_job').value
@@ -19,7 +42,21 @@ const saveButt = function(ev) {
   openPop();
 }
 
-const closeOverlay = function(ev) {
+const addPlaceButton = function (ev) {
+  ev.preventDefault()
+  let popPlace = popUpAdd.querySelector('.popup-add__input_place').value
+  let popPic = popUpAdd.querySelector('.popup-add__input_pic').value
+
+  if (popPlace === "Название" || popPic === "Ссылка на картинку") {
+    return
+  } else {
+    addPlace(popPlace, popPic);
+    popUpAddForm.reset();
+    openPopAdd();
+  }
+}
+
+const closeOverlay = function (ev) {
   if (ev.target !== ev.currentTarget) {
     return
   }
@@ -27,6 +64,77 @@ const closeOverlay = function(ev) {
 }
 
 editButton.addEventListener("click", openPop);
+addButton.addEventListener('click', openPopAdd)
 closePop.addEventListener("click", openPop);
+closePopAdd.addEventListener("click", openPopAdd);
+closePopBig.addEventListener("click", openPopBigClose);
+popSaveAdd.addEventListener("click", addPlaceButton);
 popSave.addEventListener("click", saveButt);
 popUp.addEventListener('click', closeOverlay);
+
+const initialCards = [{
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const place = document.querySelector('.places')
+const temp = document.querySelector('.template__place')
+
+
+function addPlace(name, link) {
+  const elem = temp.content.cloneNode(true);
+  elem.querySelector('.places__pic').src = link
+  elem.querySelector('.places__pic').alt = name
+  elem.querySelector('.places__name').textContent = name
+
+  elem.querySelector('.places__pic').addEventListener('click', function (ev) {
+    const picTarget = ev.target
+    const picLink = picTarget.src;
+    const placeTitleT = picTarget.closest('.places__card');
+    const placeTitle = placeTitleT.querySelector('.places__name').textContent
+    openPopBigClose();
+    openPopBig(picLink, placeTitle);
+  })
+
+
+  const trash = elem.querySelector('.places__trash')
+  const buttLike = elem.querySelector('.places__like');
+
+  buttLike.addEventListener('click', function (ev) {
+    const evTarget = ev.target;
+    evTarget.classList.toggle('places__like_active')
+  })
+
+  trash.addEventListener('click', function (ev) {
+    const cardRem = ev.target;
+    const cardRemTarget = cardRem.closest('.places__card');
+    cardRemTarget.remove();
+  })
+
+  place.prepend(elem)
+}
+
+initialCards.forEach(el => {
+  addPlace(el.name, el.link);
+})
