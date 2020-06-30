46ffd2e9b07e4f1af20719358f532876dceb8b76
R0777 <romka256@yandex.ru>
2020-06-30 10:36:35 +0300

    refactor: JS

--- js/index.js
+++ js/index.js
@@ -1,80 +1,3 @@
-const proFile = document.querySelector('.profile')
-const popUp = document.querySelectorAll('.popup')
-const editButton = proFile.querySelector('.profile__edit')
-const closePop = document.querySelectorAll('.popup__close')
-const popSave = document.querySelectorAll('.popup__save')
-const addButton = proFile.querySelector('.profile__button')
-const popUpAddForm = popUp[1].querySelector('.popup__block')
-const closePopBig = popUp[2].querySelector('.popup__close')
-const place = document.querySelector('.places')
-const template = document.querySelector('.template__place')
-
-const openPop = function (ev) {
-  const popEvt = ev.target
-  const popClass = popEvt.getAttribute('class')
-
-  if (popClass === 'profile__edit') {
-    popUp[0].classList.toggle('popup-open');
-  }
-  else if (popClass === 'profile__button') {
-  popUp[1].classList.toggle('popup-open');
-  }
-  else if (popClass === 'places__pic') {
-  popUp[2].classList.toggle('popup-open');
-  }
-  else
-  return
-}
-
-const openPopBig = function (pic, place) {
-  const popUpBigPic = popUp[2].querySelector('.popup__pic')
-  const popUpBigText = popUp[2].querySelector('.popup__place')
-  popUpBigPic.setAttribute('src', pic);
-  popUpBigPic.setAttribute('alt', place);
-  popUpBigText.innerHTML = place
-}
-
-const saveButt = function (ev) {
-  ev.preventDefault()
-  let popName = popUp[0].querySelector('.popup__input_name').value
-  let popJob = popUp[0].querySelector('.popup__input_job').value
-  let avaName = proFile.querySelector('.profile__name')
-  let avaJob = proFile.querySelector('.profile__job')
-  avaJob.textContent = popJob;
-  avaName.textContent = popName;
-  openPop();
-}
-
-const addPlaceButton = function (ev) {
-  ev.preventDefault()
-  let popPlace = popUp[1].querySelector('.popup__input_place').value
-  let popPic = popUp[1].querySelector('.popup__input_pic').value
-  addPlace(popPlace, popPic);
-  popUpAddForm.reset();
-  openPopAdd();
-
-}
-
-const closeOverlay = function (ev) {
-  if (ev.target !== ev.currentTarget) {
-    return
-  }
-  openPop();
-}
-
-editButton.addEventListener("click", openPop);
-addButton.addEventListener("click", openPop);
-
-closePop.forEach(el => {
-  el.addEventListener("click", openPop);
-})
-popSave.forEach(el => {
-  el.addEventListener("click", saveButt);
-})
-popUp.forEach(el => {
-  el.addEventListener('click', closeOverlay);
-})
-
 const initialCards = [{
     name: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
@@ -101,42 +24,128 @@ const initialCards = [{
   }
 ];
 
+const proFile = document.querySelector('.profile')
+const popUp = document.querySelectorAll('.popup')
+const editButton = proFile.querySelector('.profile__edit')
+const closePop = document.querySelectorAll('.popup__close')
+const popSave = document.querySelectorAll('.popup__save')
+const addButton = proFile.querySelector('.profile__button')
+const popUpAddForm = popUp[1].querySelector('.popup__block')
+const place = document.querySelector('.places')
+const template = document.querySelector('.template__place')
+const popUpBigPic = popUp[2].querySelector('.popup__pic')
+const popUpBigText = popUp[2].querySelector('.popup__place')
+const avaName = proFile.querySelector('.profile__name')
+const avaJob = proFile.querySelector('.profile__job')
+const inputName = popUp[0].querySelector('.popup__input_name')
+const inputJob = popUp[0].querySelector('.popup__input_job')
+const inputPlace = popUp[1].querySelector('.popup__input_place')
+const inputPic = popUp[1].querySelector('.popup__input_pic')
 
+const tooglePopUp = (event) => {
 
+  const openPopUpEvt = event.target
+  const popClass = openPopUpEvt.getAttribute('class')
 
-function addPlace(name, link) {
-  const elem = template.content.cloneNode(true);
-  elem.querySelector('.places__pic').src = link
-  elem.querySelector('.places__pic').alt = name
-  elem.querySelector('.places__name').textContent = name
-
-  elem.querySelector('.places__pic').addEventListener('click', function (ev) {
-    const picTarget = ev.target
-    const picLink = picTarget.src;
-    const placeTitleT = picTarget.closest('.places__card');
-    const placeTitle = placeTitleT.querySelector('.places__name').textContent
-    openPopBigClose();
-    openPopBig(picLink, placeTitle);
-  })
-
-
-  const trash = elem.querySelector('.places__trash')
-  const buttLike = elem.querySelector('.places__like');
-
-  buttLike.addEventListener('click', function (ev) {
-    const evTarget = ev.target;
-    evTarget.classList.toggle('places__like_active')
-  })
-
-  trash.addEventListener('click', function (ev) {
-    const cardRem = ev.target;
-    const cardRemTarget = cardRem.closest('.places__card');
-    cardRemTarget.remove();
-  })
-
-  place.prepend(elem)
+  switch (popClass) {
+    case 'profile__edit':
+      popUp[0].classList.add('popup-open');
+      break;
+    case 'profile__button':
+      popUp[1].classList.add('popup-open');
+      break;
+    case 'places__pic':
+      popUp[2].classList.add('popup-open');
+      break;
+    default:
+      popUp.forEach(element => {
+        element.classList.remove('popup-open')
+      })
+  }
 }
 
-initialCards.forEach(el => {
-  addPlace(el.name, el.link);
+const openPopBig = (pic, place, event) => {
+  popUpBigPic.setAttribute('src', pic);
+  popUpBigPic.setAttribute('alt', place);
+  popUpBigText.textContent = place
+  tooglePopUp(event)
+}
+
+const updateProfile = (event) => {
+  event.preventDefault()
+  const popName = inputName.value
+  const popJob = inputJob.value
+  avaJob.textContent = popJob;
+  avaName.textContent = popName;
+  tooglePopUp(event);
+}
+
+const addPlaceButton = (event) => {
+  event.preventDefault()
+  const popPlace = inputPlace.value
+  const popPic = inputPic.value
+  addPlace(popPlace, popPic);
+  popUpAddForm.reset();
+  tooglePopUp(event);
+}
+
+const closeOverlay = (event) => {
+  if (event.target !== event.currentTarget) {
+    return
+  }
+  popUp.forEach(element => {
+    element.classList.remove('popup-open');
+  })
+}
+
+const addCard = (card) => {
+  place.prepend(card)
+}
+
+const addPlace = (name, link) => {
+
+  const elem = template.content.cloneNode(true);
+  const placesPic = elem.querySelector('.places__pic')
+  placesPic.src = link
+  placesPic.alt = name
+  elem.querySelector('.places__name').textContent = name
+
+  placesPic.addEventListener('click', function (event) {
+    const picEvent = event.target
+    const picLink = picEvent.src;
+    const placeCard = picEvent.closest('.places__card');
+    const placeTitle = placeCard.querySelector('.places__name').textContent
+    openPopBig(picLink, placeTitle, event);
+  })
+
+  const trash = elem.querySelector('.places__trash')
+  const buttonLike = elem.querySelector('.places__like');
+
+  buttonLike.addEventListener('click', function (event) {
+    const likeTarget = event.target;
+    likeTarget.classList.toggle('places__like_active')
+  })
+
+  trash.addEventListener('click', function (event) {
+    const trashTarget = event.target;
+    const cardToRemove = trashTarget.closest('.places__card');
+    cardToRemove.remove();
+  })
+
+  addCard(elem);
+}
+
+editButton.addEventListener("click", tooglePopUp);
+addButton.addEventListener("click", tooglePopUp);
+popUp.forEach(element => {
+  element.addEventListener('click', closeOverlay);
+})
+closePop.forEach(element => {
+  element.addEventListener("click", tooglePopUp);
+})
+popSave[0].addEventListener("click", updateProfile);
+popSave[1].addEventListener("click", addPlaceButton);
+
+initialCards.forEach(element => {
+  addPlace(element.name, element.link);
 })
