1b63702b47bc0a23cd3c140390680efda48c888a
Ro777 <romka256@yandex.ru>
2020-08-17 17:36:41 +0300

    message for Aygul

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -9,7 +9,7 @@ export default class UserInfo {
     this._jobInput = document.querySelector('.popup__input_job');
     this._nameInput.value = this._name.textContent;
     this._jobInput.value = this._job.textContent;
-
+//Уважаемый Ревьюер, надеюсь, теперь я смог реализовать ту идею которую вы закладывали, если нет то прошу в комментариях объяснить подробнее. Хорошего вам дня <3))
     return {
       name: this._nameInput.value,
       job: this._jobInput.value
